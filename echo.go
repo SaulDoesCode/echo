bc247a90bbceae59befc2fb4e1cf4b8d948e5d82
Saul van der Walt <saulvdw@gmail.com>
2018-11-12 11:48:38 +0200

    use fork

--- echo.go
+++ echo.go
@@ -8,8 +8,8 @@ Example:
   import (
     "net/http"
 
-    "github.com/labstack/echo"
-    "github.com/labstack/echo/middleware"
+    "github.com/SaulDoesCode/echo"
+    "github.com/SaulDoesCode/echo/middleware"
   )
 
   // Handler
